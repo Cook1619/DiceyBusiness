1e4a11e78a76e4e1fedd9ff2e8f1fc95ae0996a1
Cook1619 <cook6348@gmail.com>
2018-06-07 08:37:38 -0500

    giving constructor values to add a div, class, and space for a dice to show up

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-let diceRoll = document.getElementById('generateDie');
-diceRoll.addEventListener("click", createDie);
-
 class Die {
-    constructor(value){
+    constructor(value, xcords, ycords, square) {
         this.value = value;
+        this.div = document.createElement('div');
+        this.div.classList.add('dice');
+        this.div.style.height = xcords;
+        this.div.style.width = ycords;
+        this.div.style.border = square;
     }
-    roll(){
+    roll() {
         return Math.floor(Math.random() * 6) + 1;
     }
 }
 
+let diceRoll = document.getElementById('generateDie');
+diceRoll.addEventListener("click", createDie);
+
+
+
 function createDie(){
-    alert('Clicked!!')
+    
 }

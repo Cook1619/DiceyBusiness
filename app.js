7208aff19459ae4ef87ed8c25462b9643998e7c7
Cook1619 <cook6348@gmail.com>
2018-06-07 15:58:24 -0500

    Figured out I didnt need to set heights and widths for the dice

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
+let diceRoll = document.getElementById('generateDie');
+diceRoll.addEventListener("click", createDie);
+
 class Die {
-    constructor(value, xcords, ycords, square) {
-        this.value = value;
+    constructor() {
         this.div = document.createElement('div');
         this.div.classList.add('dice');
-        this.div.style.height = xcords;
-        this.div.style.width = ycords;
-        this.div.style.border = square;
     }
     roll() {
         return Math.floor(Math.random() * 6) + 1;
     }
 }
 
-let diceRoll = document.getElementById('generateDie');
-diceRoll.addEventListener("click", createDie);
-
-
-
 function createDie(){
-    
+    let die1 = new Die();
 }
